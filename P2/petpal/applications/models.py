09121d9c30423dc9ad98a0dd4cdcfb1654e5752b
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-11-10 15:25:24 -0500

    application model

--- P2/petpal/applications/models.py
+++ P2/petpal/applications/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from pets.models import PetPost
+from accounts.models import CustomUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Application(models.Model):
+    STATUS_CHOICE = (
+        ("pending", "Pending"),
+        ("accepted", "Accepted"),
+        ("denied", "Denied"),
+        ("withdrawn", "Withdrawn"),
+    )
+    petpost = models.ForeignKey(PetPost, on_delete=models.CASCADE)
+    seeker = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="seekers"
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    adopter_firstname = models.CharField(max_length=255)
+    adopter_lastname = models.CharField(max_length=255)
+    adopter_email = models.EmailField()
+
+    co_adopter_firstname = models.CharField(max_length=255, null=True, blank=True)
+    co_adopter_lastname = models.CharField(max_length=255, null=True, blank=True)
+    co_adopter_email = models.EmailField(null=True, blank=True)
+
+    addr_street = models.CharField(max_length=255, verbose_name="street")
+    addr_city = models.CharField(max_length=255, verbose_name="city")
+    addr_province = models.CharField(max_length=10, verbose_name="province")
+    addr_postal = models.CharField(max_length=10, verbose_name="postal")
+
+    phone = PhoneNumberField(null=False, blank=False)
+    phone_type = models.CharField(
+        max_length=10, choices=(("cell", "Cell"), ("home", "Home"))
+    )
+
+    have_pet_curretly = models.BooleanField(verbose_name="Currently have any pets?")
+    have_pet_notes = models.TextField(
+        null=True, blank=True, verbose_name="Please list your current pets"
+    )
+
+    family_members = models.TextField(
+        verbose_name="Names and ages of all permanent residents of your home (adults/children)"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.seeker} wants {self.petpost}"
