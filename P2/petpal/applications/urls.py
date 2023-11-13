5800d9083d4799b3e84499ae20b9e3e8eceb84de
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-11-12 21:02:23 -0500

    fix typo

--- P2/petpal/applications/urls.py
+++ P2/petpal/applications/urls.py
@@ -5,6 +5,6 @@ app_name = "application"
 urlpatterns = [
     path("seeker/", views.SeekerApplicationList.as_view(), name="list_all"),
     path("seeker/<int:id>/", views.SeekerApplicationDetial.as_view(), name="detail"),
-    path("shelter/", views.ShlterApplicationList.as_view(), name="list_all"),
-    path("shelter/<int:id>/", views.ShlterApplicationDetial.as_view(), name="detail"),
+    path("shelter/", views.ShelterApplicationList.as_view(), name="list_all"),
+    path("shelter/<int:id>/", views.ShelterApplicationDetial.as_view(), name="detail"),
 ]
