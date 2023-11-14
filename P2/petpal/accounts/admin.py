efa84a681d10b578d3fc66b7c77ed6b7eab26582
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-11-14 17:48:50 -0500

    set accounts admin panel

--- P2/petpal/accounts/admin.py
+++ P2/petpal/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
+
 
 # Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("username", "id", "role")
+
+
+admin.site.register(CustomUser, AccountAdmin)
