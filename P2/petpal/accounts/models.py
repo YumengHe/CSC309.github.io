fdf5947a04084757d092dc85f37bb18044052e04
Harry <harry.ye@mail.utoronto.ca>
2023-11-14 22:41:18 -0500

    add migration folder and __init__.py

--- P2/petpal/accounts/models.py
+++ P2/petpal/accounts/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     address = models.CharField(max_length=100, null=True, blank=True)
     # role can be one of "seeker" or "shelter"
     role = models.CharField(max_length=10)
+    profile_pic = models.ImageField(upload_to="user_profiles/")
 
     def __str__(self):
         return self.email  # or username if you are using that
