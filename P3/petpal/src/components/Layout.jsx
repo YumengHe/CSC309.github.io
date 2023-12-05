b78df1f7cf5edfd8fd5c8dbdd23e392f66a6fec8
Harry <harry.ye@mail.utoronto.ca>
2023-12-05 11:03:07 -0500

    update notification logic

--- P3/petpal/src/components/Layout.jsx
+++ P3/petpal/src/components/Layout.jsx
@@ -9,36 +9,36 @@ const Layout = ({ children }) => {
   const [notification, setNotification] = useState(null);
   const [showNotificationPopup, setShowNotificationPopup] = useState(false);
 
-  const fetchNotifications = async () => {
-    if ((await isUserLoggedIn()) === true) {
-      try {
-        const response = await fetchWithToken("/notifications/?state=unread"); // Update with your actual API endpoint
-        if (response.status === 200) {
-          const data = await response.json();
-          const newNotifications = data?.results[0];
-          setNotification(newNotifications);
-          // console.log(
-          //   "newNotification:",
-          //   newNotifications,
-          // );
-          if (newNotifications) {
-            setShowNotificationPopup(true); // Show pop-up if there are new notification
-          }
-        } else if (response.status === 204) {
-          // console.log("No new notification");
-        }
-      } catch (error) {
-        console.error("Error fetching notification:", error);
-      }
-    } else {
-      // console.log("User not logged in");
-    }
-  };
-
   useEffect(() => {
-    const interval = setInterval(fetchNotifications, 1000); // Poll every 10 seconds
+    const fetchNotifications = async () => {
+      // Only fetch notifications if (there are no notifications or the notification popup is not shown) and (the user is logged in)
+      if (
+        (!showNotificationPopup || !notification) &&
+        (await isUserLoggedIn())
+      ) {
+        try {
+          const response = await fetchWithToken("/notifications/?state=unread"); // Update with your actual API endpoint
+          if (response.status === 200) {
+            const data = await response.json();
+            const newNotifications = data?.results[0];
+            setNotification(newNotifications);
+            if (newNotifications) {
+              setShowNotificationPopup(true); // Show pop-up if there are new notifications
+            }
+          } else if (response.status === 204) {
+            // Handle no new notifications
+          }
+        } catch (error) {
+          console.error("Error fetching notification:", error);
+        }
+      } else {
+        // Handle user not logged in
+      }
+    };
+
+    const interval = setInterval(fetchNotifications, 1000); // Poll every 1 second
     return () => clearInterval(interval); // Cleanup on unmount
-  }, []);
+  }, [showNotificationPopup]); // Add relevant dependencies here
 
   const handleClosePopup = async () => {
     try {
