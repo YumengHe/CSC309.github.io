63f25fe8e80cd578c0f662a95ba7909235a882ff
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-12-07 20:05:28 -0500

    paginate support

---
+++ P3/petpal/src/pages/Application/Paginate.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import React from "react";
+import "./style.css";
+
+const Paginate = ({ totalPages, currentPage, paginate }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <div className="col col-12 mx-auto">
+            <ul className="d-flex justify-content-center gap-3">
+                <li
+                    className="btn btn-outline-primary-cust"
+                    onClick={() =>
+                        currentPage > 1 ? paginate(currentPage - 1) : pass
+                    }
+                >
+                    &lt;
+                </li>
+                {pageNumbers.map((number) => (
+                    <li
+                        key={number}
+                        onClick={() => paginate(number)}
+                        className={`btn btn-outline-primary-cust 
+                            ${currentPage === number ? `active` : ``}`}
+                    >
+                        {number}
+                    </li>
+                ))}
+                <li
+                    className="btn btn-outline-primary-cust"
+                    onClick={() =>
+                        currentPage < totalPages
+                            ? paginate(currentPage + 1)
+                            : pass
+                    }
+                >
+                    &gt;
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Paginate;
