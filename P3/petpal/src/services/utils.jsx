e2f7947f973fac5f0f14f2b5fff41cb9beef720c
Raymond Ye <raymondye@raymonds-mini.home>
2023-12-13 11:40:14 -0500

    utils

--- P3/petpal/src/services/utils.jsx
+++ P3/petpal/src/services/utils.jsx
@@ -76,3 +76,49 @@ export const fetchWithoutToken = async (url, method = "GET", data = null) => {
     return response;
   });
 };
+
+export const fetchWithTokenWithFullUrl = async (
+  url,
+  method = "GET",
+  data = null,
+) => {
+  const fullUrl = `${url}`;
+  const accessToken = localStorage.getItem("accessToken");
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+  if (accessToken === null) {
+    // If there's no access token, make a request without a token
+    return await fetchWithoutToken(url, method, data);
+  }
+
+  if (!(data instanceof FormData)) {
+    headers["Content-Type"] = "application/json";
+  }
+
+  const config = {
+    headers,
+    method,
+  };
+
+  // Append data to the configuration based on whether data is present
+  if (data) {
+    config.body =
+      data instanceof FormData || typeof data === "string"
+        ? data
+        : JSON.stringify(data);
+  }
+
+  console.log(
+    "sending request to:",
+    fullUrl,
+    "with config:",
+    config,
+    "with data:",
+    data instanceof FormData ? Object.fromEntries(data) : data,
+  );
+
+  return fetch(fullUrl, config).then((response) => {
+    return response;
+  });
+};
