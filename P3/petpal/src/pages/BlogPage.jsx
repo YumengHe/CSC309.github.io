5b2a554ce0659c17b7cf24a36ae51e1b93834d37
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-12-13 12:44:07 -0500

    fix bug

--- P3/petpal/src/pages/BlogPage.jsx
+++ P3/petpal/src/pages/BlogPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchWithToken } from "../services/utils";
+import BlogComments from "./BlogComment";
 
 const BlogPage = () => {
   const { id } = useParams();
