468e3161659b925d3f86ac464447d2d3fbb5bb8f
Chan Yu <chanxq.yu@mail.utoronto.ca>
2023-12-19 19:53:52 -0500

    delete unused

--- backend/petpal/blogComments/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import BlogComment
-
-
-class BlogCommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogComment
-        fields = ['id', 'text', 'created_at']
-        read_only_fields = ('id', 'created_at')
--- backend/petpal/blogComments/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BlogCommentsView
-
-urlpatterns = [
-    path('<int:blog_id>/', BlogCommentsView.as_view(), name='blog-comments'),
-]
